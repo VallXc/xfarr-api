bdde26d30c7eb3935e58a680ab36fb0ba6311329
VallXc <95421124+VallXc@users.noreply.github.com>
2022-02-08 10:13:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
 const request = require('request')
-const author = "#𝑿𝑭𝒂𝒓"
+const author = "Zynfx"
 
 const film = (query) => {
 	return new Promise((resolve, reject) => {
